abc56e7b76db00bc32342ab13ccd368afcaeda06
Jonah Ale <jonahale94@gmail.com>
2026-01-05 13:37:21 -0800

    feat: add user controller and exception handling
     - Add UserController with CRUD endpoints
     - Update User entity fields
     - Add custom exceptions in exceptions package

---
+++ src/main/java/com/jonah/planner/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.jonah.planner.controller;
+
+import com.jonah.planner.domain.User;
+import com.jonah.planner.exception.UserNotFoundException;
+import com.jonah.planner.repository.UserRepository;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+public class UserController {
+    private final UserRepository userrepository;
+
+    UserController(UserRepository userRepository) {
+        this.userrepository = userRepository;
+    }
+
+    @GetMapping("/users")
+    List<User> all() {
+        return userrepository.findAll();
+    }
+
+    @PostMapping("/users")
+    User newUser(@RequestBody User newUser) {
+        return userrepository.save(newUser);
+    }
+
+    @GetMapping("/users/{id}")
+    User one(@PathVariable Long id) {
+        return userrepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @PutMapping("/users/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userrepository.findById(id)
+                .map(user -> {
+                    user.setName(newUser.getName());
+                    user.setRoles(newUser.getRoles());
+                    return userrepository.save(user);
+                })
+                .orElseGet(() -> {
+                    return userrepository.save(newUser);
+                });
+    }
+
+    @DeleteMapping("/users/{id}")
+    void deleteUser(@PathVariable Long id) {
+        userrepository.deleteById(id);
+    }
+
+}
--- src/main/java/com/jonah/planner/domain/User.java
+++ src/main/java/com/jonah/planner/domain/User.java
@@ -3,17 +3,31 @@ package com.jonah.planner.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class User {
+    @Getter
     @Id
     private long id;
+
+    @Getter
     private String password;
+
+    @Getter
+    @Setter
     private String name;
+
+    @Getter
+    @Setter
     private String email;
+
+    @Getter
+    @Setter
     private List<Role> roles;
 }
